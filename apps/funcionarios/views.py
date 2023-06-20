6372ccaeb10ea7879b48cd89040723823369f3b3
Leandro <franciscoleandrosilvacoutinho@gmail.com>
2023-06-19 22:28:22 -0300

    listar funcionarios

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -3,3 +3,7 @@ from .models import Funcionario
 
 class FuncionarioList(ListView):
     model = Funcionario
+
+    def get_queryset(self):
+        empresa_logada=self.request.user.funcionario.empresa
+        return Funcionario.objects.filter(empresa=empresa_logada)
