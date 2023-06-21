5a0332a666b98a1321cdf6c6e058278b77f55f46
Leandro <franciscoleandrosilvacoutinho@gmail.com>
2023-06-21 10:38:50 -0300

    delete funcionario

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import FuncionarioList
+from .views import FuncionarioList, FuncionarioEdit, FuncionarioDelete
 
 urlpatterns = [
     path('',FuncionarioList.as_view(), name = 'list_funcionarios' ),
+    path('editar/<int:pk>/',FuncionarioEdit.as_view(), name = 'edit_funcionarios' ),
+    path('deletar/<int:pk>/',FuncionarioDelete.as_view(), name = 'delete_funcionarios' ),
 ]
--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Funcionario
 
 class FuncionarioList(ListView):
@@ -7,3 +8,11 @@ class FuncionarioList(ListView):
     def get_queryset(self):
         empresa_logada=self.request.user.funcionario.empresa
         return Funcionario.objects.filter(empresa=empresa_logada)
+
+class FuncionarioEdit(UpdateView):
+    model = Funcionario
+    fields = ['nome', 'departamentos']
+
+class FuncionarioDelete(DeleteView):
+    model = Funcionario
+    success_url = reverse_lazy("list_funcionarios")
